8ae709f338a4ace925b7cc29e77640256a77e302
shohagpink-gif <shohagpink@gmail.com>
2026-05-29 15:04:48 +0600

    Batch 3: UI and Main Activity

---
+++ app/src/main/java/com/paybd/app/ui/main/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.paybd.app.ui.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.paybd.app.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
